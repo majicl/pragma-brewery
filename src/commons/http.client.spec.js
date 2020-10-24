f4a6f3ccac017b7bf8d87442850e88885b01f5fe
Majid Parvin <parvin.majid@live.com>
2020-10-24 22:11:29 +0200

    introduce http.client with its unit-tests

---
+++ src/commons/http.client.spec.js
@@ -0,0 +1,31 @@
+import chai from 'chai';
+import sinon from 'sinon';
+import axios from 'axios';
+import HttpClient from './http.client.js';
+
+const expect = chai.expect;
+describe('HttpClient', function () {
+    let axiosStub;
+    before(() => {
+        axiosStub = sinon.stub(axios, 'get').returns(
+            Promise.resolve({
+                data: {
+                    id: 1,
+                    temperature: -2,
+                },
+            })
+        );
+    });
+    after(() => {
+        axiosStub.restore();
+    });
+    describe('get', () => {
+        it('the httpClient get function should be called with the expected response', async () => {
+            var result = await HttpClient.get('https://www.google.com');
+            expect(result).to.have.property('data').equal({
+                id: 1,
+                temperature: -2,
+            });
+        });
+    });
+});
