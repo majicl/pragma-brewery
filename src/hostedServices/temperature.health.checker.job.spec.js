846401f3a43b751fb83d5ec502e7dc7cd567c5ce
Majid Parvin <parvin.majid@live.com>
2020-10-25 00:22:32 +0200

    incude socket and getBeersOutsideTemperatureIds functionality in the test

--- src/hostedServices/temperature.health.checker.job.spec.js
+++ src/hostedServices/temperature.health.checker.job.spec.js
@@ -2,17 +2,34 @@ import chai from 'chai';
 import sinon from 'sinon';
 import JobService from './job.service.js';
 import temperatureHealthChecker from './temperature.health.checker.job.js';
+import socket from '../socket.js';
+import BeerServices from './../services/beer.services.js';
 
 const expect = chai.expect;
 describe('JobService', () => {
-    let jobStub;
-    before(() => (jobStub = sinon.spy(JobService, 'run')));
-    after(() => jobStub.restore());
+    let jobSpy;
+    let socketSpy;
+    let beerServicesSpy;
+    before(() => {
+        jobSpy = sinon.spy(JobService, 'run');
+        socketSpy = sinon.spy(socket, 'getActiveSocket');
+        beerServicesSpy = sinon.spy(BeerServices, 'getBeersOutsideTemperatureIds');
+    });
+    after(() => {
+        jobSpy.restore();
+        socketSpy.restore();
+        beerServicesSpy.restore();
+    });
 
     describe('temperatureHealthChecker', () => {
-        it('temperatureHealthChecker -> job.service.run has been called', async () => {
+        it('temperatureHealthChecker -> job.service.run has been called', (done) => {
             temperatureHealthChecker();
-            sinon.assert.called(jobStub);
+            sinon.assert.called(jobSpy);
+            setTimeout(() => {
+                sinon.assert.called(socketSpy);
+                sinon.assert.called(beerServicesSpy);
+                done();
+            }, 1000);
         });
     });
 });
