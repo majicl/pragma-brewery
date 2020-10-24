9abe12ba6f0c1201dc278ee891b15a106eb481af
Majid Parvin <parvin.majid@live.com>
2020-10-24 21:16:03 +0200

    introduce app.config.js

---
+++ src/config/app.config.js
@@ -0,0 +1,62 @@
+const config = {};
+
+config.web = {};
+config.services = {
+    beers: {
+        temperatureSensorServiceBaseURL:
+            process.env.SERVICES_BEERS_BASEURL ||
+            'https://temperature-sensor-service.herokuapp.com/sensor/',
+        types: [
+            {
+                id: 1,
+                name: 'IPA',
+                temperature: {
+                    min: 4,
+                    max: 6,
+                },
+            },
+            {
+                id: 2,
+                name: 'Pilsner',
+                temperature: {
+                    min: 5,
+                    max: 6,
+                },
+            },
+            {
+                id: 3,
+                name: 'Lager',
+                temperature: {
+                    min: 4,
+                    max: 7,
+                },
+            },
+            {
+                id: 4,
+                name: 'Stout',
+                temperature: {
+                    min: 6,
+                    max: 8,
+                },
+            },
+            {
+                id: 5,
+                name: 'Wheat Beer',
+                temperature: {
+                    min: 3,
+                    max: 5,
+                },
+            },
+            {
+                id: 6,
+                name: 'Pale Ale',
+                temperature: {
+                    min: 4,
+                    max: 6,
+                },
+            },
+        ],
+    },
+};
+
+export default Object.freeze(config);
