a14f67449e0f1820f9e6b873ac8dcbcda17f028b
Majid Parvin <parvin.majid@live.com>
2020-10-26 21:42:55 +0100

    config scss loader

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -37,6 +37,10 @@ module.exports = {
         options: {
           presets: ['@babel/preset-react']
         }
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
       }
     ]
   },
