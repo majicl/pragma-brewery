db396ed2274481728b4c3e7cecfab4b161c0cf9c
Majid Parvin <parvin.majid@live.com>
2020-10-26 14:10:59 +0100

    add registerServiceWorker

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-ReactDOM.render(<div>main</div>, document.getElementById("root"));
+import registerServiceWorker from './registerServiceWorker.js';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+registerServiceWorker();
