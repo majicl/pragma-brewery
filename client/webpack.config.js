05337de1b93abad528df596ebea334e8007c65b8
Majid Parvin <parvin.majid@live.com>
2020-10-26 11:03:40 +0100

    add jsx to the alias resolver

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -41,13 +41,13 @@ module.exports = {
     ]
   },
   resolve: {
-		modules: ['node_modules'],
-		extensions: ['.ts', '.tsx', '.js', '.json'],
-		alias: {
-			'~': path.resolve(__dirname, 'src/components'),
-			handlebars: 'handlebars/dist/handlebars.js'
-		}
-	},
+    modules: ['node_modules'],
+    extensions: ['.jsx', '.js', '.json'],
+    alias: {
+      '~': path.resolve(__dirname, 'src/components'),
+      handlebars: 'handlebars/dist/handlebars.js'
+    }
+  },
   devtool: 'source-map',
   plugins: [htmlPlugin, copyPlugin]
 };
