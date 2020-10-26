a1785576e678632cbc26b71f75d423d50b155e4f
Majid Parvin <parvin.majid@live.com>
2020-10-26 10:46:14 +0100

    add test configuration

---
+++ client/jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  verbose: true,
+  automock: false,
+  globals: {
+    NODE_ENV: 'test'
+  },
+  transform: {
+    '\\.[jt]sx?$': 'babel-jest'
+  },
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)'],
+  testURL: 'http://localhost',
+  moduleNameMapper: {
+    '~(.*)$': '<rootDir>/src/$1',
+    '\\.(css|scss|sass)$': '<rootDir>/__mocks__/styleMock.js'
+  }
+};
